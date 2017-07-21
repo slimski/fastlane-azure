5f5267416205f8c2f2db9e4c9d8e753092dec597
Nick Wallin <nick.wallin@sebrands.com>
2017-07-21 10:05:52 -0500

    upload blob instead of file

--- fastlane/actions/azure.rb
+++ fastlane/actions/azure.rb
@@ -172,7 +172,7 @@ module Fastlane
               html_file_name = File.basename(params[:deploy_html])
               html_azure_path = File.join(params[:path], html_file_name)
               html_azure_url = "https://#{params[:acount_name]}.blob.core.windows.net/#{params[:container]}/#{html_azure_path}"
-              upload_file(blobs, params[:container], html_azure_path, deploy_render)
+              blobs.create_block_blob(params[:container], html_azure_path, deploy_render)
             end
 
             plist_template = eth.load_from_path(params[:plist_template])
